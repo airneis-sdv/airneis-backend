b99971389921079e4283a53d137e9c093b2e7731
kennymnh <93839333+kennymnh@users.noreply.github.com>
2024-03-06 19:47:33 +0100

    Remove default project files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './config/database.module';
 
@@ -9,7 +7,7 @@ import { DatabaseModule } from './config/database.module';
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
