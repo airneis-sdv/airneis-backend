8f890335de602fa1dfa55c13ae4761604ebacae2
kennymnh <93839333+kennymnh@users.noreply.github.com>
2024-03-11 09:26:50 +0100

    Add authorization to admin users route

--- src/user/users.controller.ts
+++ src/user/users.controller.ts
@@ -1,5 +1,7 @@
 import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Patch, Post, Query, UseInterceptors } from "@nestjs/common";
 import { ApiQuery } from "@nestjs/swagger";
+import { Authorize } from "src/auth/decorators/authorize.decorator";
+import { Role } from "src/auth/enums/role.enum";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { UsersService } from "./users.service";
@@ -11,12 +13,14 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
+  @Authorize(Role.ADMIN)
   async create(@Body() createUserDto: CreateUserDto) {
     const user = await this.usersService.create(createUserDto);
     return { success: true, user };
   }
 
   @Get()
+  @Authorize(Role.ADMIN)
   @ApiQuery({ name: "search", required: false })
   async findAll(@Query() query: { search?: string }) {
     const users = query.search
@@ -27,12 +31,14 @@ export class UsersController {
   }
 
   @Get(":id")
+  @Authorize(Role.ADMIN)
   async findOne(@Param("id") id: string) {
     const user = await this.usersService.findOne(+id);
     return { success: true, user };
   }
 
   @Patch(":id")
+  @Authorize(Role.ADMIN)
   async update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
     await this.usersService.update(+id, updateUserDto);
 
@@ -41,6 +47,7 @@ export class UsersController {
   }
 
   @Delete(":id")
+  @Authorize(Role.ADMIN)
   async remove(@Param("id") id: string) {
     await this.usersService.remove(+id);
     return { success: true };
