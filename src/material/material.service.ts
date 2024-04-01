966ba2ff6d764fbc58db59349c75b5affbdc4ed3
kennymnh <93839333+kennymnh@users.noreply.github.com>
2024-04-01 21:32:00 +0200

    Fix wrong repo name from copy-paste

--- src/material/material.service.ts
+++ src/material/material.service.ts
@@ -6,33 +6,33 @@ import { Material } from "./entities/material.entity";
 
 @Injectable()
 export class MaterialService {
-  constructor(@InjectRepository(Material) private mediaRepository: Repository<Material>) { }
+  constructor(@InjectRepository(Material) private materialRepository: Repository<Material>) { }
 
   create(materialDto: MaterialDto) {
-    const material = this.mediaRepository.create(materialDto);
-    return this.mediaRepository.save(material);
+    const material = this.materialRepository.create(materialDto);
+    return this.materialRepository.save(material);
   }
 
   findAll() {
-    return this.mediaRepository.find();
+    return this.materialRepository.find();
   }
 
   async findOne(id: number) {
-    const material = await this.mediaRepository.findOne({ where: { id } });
+    const material = await this.materialRepository.findOne({ where: { id } });
     if (!material) throw new NotFoundException(`Material with id ${id} not found`);
 
     return material;
   }
 
   async update(id: number, materialDto: MaterialDto) {
-    const result = await this.mediaRepository.update(id, materialDto);
+    const result = await this.materialRepository.update(id, materialDto);
 
     if (result.affected !== 1)
       throw new NotFoundException(`Material with id ${id} not found`);
   }
 
   async remove(id: number) {
-    const result = await this.mediaRepository.delete(id);
+    const result = await this.materialRepository.delete(id);
 
     if (result.affected !== 1)
       throw new NotFoundException(`Material with id ${id} not found`);
