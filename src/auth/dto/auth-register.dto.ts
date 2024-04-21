69fed2c748b0516271aead4649249f30da51bdbb
kennymnh <93839333+kennymnh@users.noreply.github.com>
2024-04-21 22:33:42 +0200

    Set minimum password length on registration route

--- src/auth/dto/auth-register.dto.ts
+++ src/auth/dto/auth-register.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsBoolean, IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsBoolean, IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
 
 export class AuthRegisterDto {
   @ApiProperty()
@@ -14,6 +14,7 @@ export class AuthRegisterDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @MinLength(8)
   public password: string;
 
   @ApiProperty()
